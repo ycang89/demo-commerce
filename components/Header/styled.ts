cd3f277fb11600498aade9d341889603320b8ea4
Ang Yc <yeowchuan89@gmail.com>
2022-06-19 12:45:26 +0800

    fix: minor fix on ui

--- components/Header/styled.ts
+++ components/Header/styled.ts
@@ -1,8 +1,12 @@
 import { styled } from "@mui/material/styles";
 
-export const LogoWrapper = styled("div")(() => ({
+export const LogoWrapper = styled("div")((props) => ({
   flexGrow: 1,
-  textAlign: "center",
+  textAlign: "left",
+  [props.theme.breakpoints.up("md")]: {
+    textAlign: "center",
+    paddingLeft: 130
+  }
 }));
 
 export const LogoImageWrapper = styled("a")(() => ({
